512ebc2bde111538ad6917f342f40bf953e6fdf5
AlvinOne24 <al24vin@mhs.mdp.ac.id>
2023-04-05 17:14:43 +0700

    Ubah Data

--- app/src/main/java/com/example/payobejalan/AdapterDestinasi.java
+++ app/src/main/java/com/example/payobejalan/AdapterDestinasi.java
@@ -2,6 +2,7 @@ package com.example.payobejalan;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -68,7 +69,20 @@ public class AdapterDestinasi extends RecyclerView.Adapter<AdapterDestinasi.View
                     pesan.setPositiveButton("Ubah", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
+                            //rabu
+                            String id,nama, alamat, jam;
 
+                            id = tvId.getText().toString();
+                            nama = tvNama.getText().toString();
+                            alamat = tvAlamat.getText().toString();
+                            jam = tvJam.getText().toString();
+
+                            Intent kirim = new Intent(ctx,UbahActivity.class);
+                            kirim.putExtra("xId",id);
+                            kirim.putExtra("xNama",nama);
+                            kirim.putExtra("xAlamat",alamat);
+                            kirim.putExtra("xJam",jam);
+                            ctx.startActivity(kirim);
                         }
                     });
 
